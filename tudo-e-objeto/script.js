daf416eb3732999189d6c0db166bb3eb5765b3bc
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-27 13:55:19 -0300

    tudo e objeto conteudo e exercicio

---
+++ tudo-e-objeto/script.js
@@ -0,0 +1,21 @@
+const nome = 'André';
+
+const nomeMinusculo = nome.toLowerCase();
+
+const btn = document.querySelector('.btn');
+
+// nomeie 3 propriedades ou métodos de strings
+nome.length;
+console.log(nome.fixed());
+nome.toUpperCase();
+nome.replace('ré', 'rei');
+// nomeie 5 propriedades ou métodos de elementos do DOM
+// document.querySelector
+// document.createElement
+// document.appendChild
+// document.innerHTML
+// document.innerText
+
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
