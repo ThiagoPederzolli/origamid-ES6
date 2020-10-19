29f2f89887cc3e01bd001dfb41289c29bbf73c26
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-19 17:54:13 -0300

    aula2 prototype finalizada

--- objetos-parte2/prototypes/script.js
+++ objetos-parte2/prototypes/script.js
@@ -15,7 +15,7 @@ function Pessoa(nome, idade) {
     return 'Andou pelo Objeto';
   };
 };
-console.log(Pessoa.prototype);
+// console.log(Pessoa.prototype);
 
 Pessoa.prototype.andar = function() {
   return this.nome + ' andou';
@@ -23,9 +23,37 @@ Pessoa.prototype.andar = function() {
 Pessoa.prototype.nadar = function() {
   return this.nome + ' nadou';
 }
-console.log(Pessoa.prototype); // retorna o objeto
+// console.log(Pessoa.prototype); // retorna o objeto
 
 
 const thiago = new Pessoa('Thiago', 30);
 
-console.log(thiago.prototype);
+// console.log(thiago.prototype);
+
+const pais = 'Brasil';
+const cidade = new String('Rio');
+
+pais.charAt(0); // B
+cidade.charAt(0); // R
+
+String.prototype;
+
+const listaAnimais = ['Cachorro', 'Gato', 'Cavalo'];
+
+const lista = document.querySelectorAll('li');
+// console.log(lista)
+// Transforma em uma array
+const listaArray1 = Array.prototype.slice.call(lista);
+const listaArray2 = Array.from(lista);
+
+// console.log(listaArray1)
+// console.log(listaArray2)
+
+const Carro = {
+  marca: 'Ford',
+  preco: 2000,
+  acelerar() {
+    return true;
+  }
+}
+
