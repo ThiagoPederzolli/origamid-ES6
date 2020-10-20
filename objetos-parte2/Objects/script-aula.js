984065f7080ae9bfa67d41836375c909ee37dee1
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-20 20:31:07 -0300

    conteudo aula 3 Objects

--- objetos-parte2/Objects/script-aula.js
+++ objetos-parte2/Objects/script-aula.js
@@ -65,8 +65,8 @@ Object.defineProperties(moto, {
 
 console.log(moto);
 
-const frutas = ['Banana', 'Pêra']
-Object.getPrototypeOf(frutas);
+const frutas3 = ['Banana', 'Pêra']
+Object.getPrototypeOf(frutas3);
 Object.getPrototypeOf(''); // String
 
 const frutas1 = ['Banana', 'Pêra'];
@@ -74,18 +74,43 @@ const frutas2 = ['Banana', 'Pêra'];
 
 Object.is(frutas1, frutas2); // false
 
-const carro = {
+const carro5 = {
   marca: 'Ford',
   ano: 2018,
 }
-// Object.freeze(carro);
-// Object.seal(carro);
-Object.preventExtensions(carro);
+// Object.freeze(carro5);
+// Object.seal(carro5);
+Object.preventExtensions(carro5);
 
-// Object.isFrozen(carro); // true
-// Object.isSealed(carro); // true
-// Object.isExtensible(carro); // true
-carro.marca = 'Fiat';
-carro.portas = 4;
+// Object.isFrozen(carro5); // true
+// Object.isSealed(carro5); // true
+// Object.isExtensible(carro5); // false
+carro5.marca = 'Fiat';
+carro5.portas = 4;
 
-console.log(carro)
+console.log(carro5)
+
+const frutas4 = ['Banana', 'Uva'];
+frutas4.constructor; // Array
+
+const frase2 = 'Isso é uma String';
+frase2.constructor; // String
+
+const frutas = ['Banana', 'Uva'];
+frutas.toString(); // 'Banana,Uva'
+typeof frutas; // object
+Object.prototype.toString.call(frutas); // [object Array]
+
+const frase = 'Uma String';
+frase.toString(); // 'Uma String'
+typeof frase; // string
+Object.prototype.toString.call(frase); // [object String]
+
+const carro = {marca: 'Ford'};
+carro.toString(); // [object Object]
+typeof carro; // object
+Object.prototype.toString.call(carro); // [object Object]
+
+const li = document.querySelectorAll('li');
+typeof li; // object
+Object.prototype.toString.call(li); // [object NodeList]
