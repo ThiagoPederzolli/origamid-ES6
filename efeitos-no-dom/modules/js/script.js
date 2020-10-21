84b523342a88c2c287ebdedf7665b7d0d70bc0f7
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-21 09:31:30 -0300

    separando modules das funcoes JS

---
+++ efeitos-no-dom/modules/js/script.js
@@ -0,0 +1,8 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/animacao-por-scroll.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tab-nav.js';
+initTabNav();
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
