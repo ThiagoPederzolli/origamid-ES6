a9d502c60b4a9b90ed261b1f3c06470f291baf18
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-21 09:57:35 -0300

    concluida aula e exericicios modules

--- efeitos-no-dom/proxima/js/script.js
+++ efeitos-no-dom/proxima/js/script.js
@@ -2,14 +2,8 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/animacao-por-scroll.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
-import * as teste from './modules/teste.js';
-
-// console.log(meuNome);
 
 initTabNav();
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
-teste.teste1();
-teste.teste2();
-console.log(teste.senha);
