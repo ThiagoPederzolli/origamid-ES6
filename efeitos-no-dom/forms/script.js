7a1fe41aae03750b8ac6e44b20df28ff6d3f2670
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-22 12:41:34 -0300

    conclusao aulas forms

--- efeitos-no-dom/forms/script.js
+++ efeitos-no-dom/forms/script.js
@@ -1,6 +1,7 @@
 const contato = document.querySelector('#contato');
-console.log(contato.elements)
-console.log(document.forms[0]);
+const texto = document.querySelector('.texto');
+// console.log(contato.elements)
+// console.log(document.forms[0]);
 
 const form = document.getElementById('contato');
 function handleKeyUp(event) {
@@ -12,9 +13,31 @@ function handleChange(event) {
   const target = event.target;
   if(!target.checkValidity()) {
     target.classList.add('invalido');
+    contato.nome.setCustomValidity('Nome é totalmente necessário!')
     target.nextElementSibling.innerText = target.validationMessage;
-  } else {
-    target.classList.remove('invalido');
-  }
+  } 
+  texto.innerText = event.target.value;
+}
+form.addEventListener('change', handleChange);
+
+// function handleChange(event) {
+//   document.body.style.backgroundColor = event.target.value;
+// }
+// form.addEventListener('change', handleChange);
+
+// function handleChange(event) {
+//   console.log(event.target.value)
+// }
+// form.addEventListener('change', handleChange);
+
+// function handleChange(event) {
+//   if(event.target.checked)
+//     console.log(event.target.value);
+// }
+// form.addEventListener('change', handleChange);
+
+const dados = {};
+function handleChange(event) {
+  dados[event.target.name] = event.target.value;
 }
 form.addEventListener('change', handleChange);
