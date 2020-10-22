126e3f2b0ea5c64dca9c6cff2e2b35567562a7c7
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-21 23:12:42 -0300

    conteudo aula 1 menu mobile

---
+++ efeitos-no-dom/menu-mobile/js/script.js
@@ -0,0 +1,16 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/animacao-por-scroll.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tab-nav.js';
+import initModal from './modules/modal.js';
+import initToolTip from './modules/tooltip.js';
+import initDropDownMenu from './modules/dropdown-menu.js';
+
+initTabNav();
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initModal();
+initToolTip();
+initDropDownMenu(); 
+
