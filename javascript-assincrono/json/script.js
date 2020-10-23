e43db647aa1db27e9c5676220fe508244faacfca
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-23 10:36:30 -0300

    conteudo sobre JSON

---
+++ javascript-assincrono/json/script.js
@@ -0,0 +1,24 @@
+// fetch('./dados.json')
+// .then(r => r.json())
+// .then(json => {
+//   json.forEach(materia => {
+//     console.log(materia.aula);
+//   })
+// })
+
+fetch('./dados.json')
+.then(r => r.text())
+.then(jsonText => {
+ const jsonFinal = JSON.parse(jsonText);
+ console.log(jsonFinal);
+})
+
+const configuracoes = {
+  player: "Google API",
+  tempo: 25.5,
+  aula: "2-1 JavaScript",
+  vitalicio: true,
+}
+
+localStorage.configuracoes = JSON.stringify(configuracoes);
+const pegarConfiguracoes = JSON.parse(localStorage.configuracoes);
