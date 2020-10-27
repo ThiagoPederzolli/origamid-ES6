17e725e2204677860ab265a0eb1225dcdd3eee50
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-26 21:53:06 -0300

    exercicios destructuring

--- destructuring/script.js
+++ destructuring/script.js
@@ -1,73 +1,25 @@
-const carro = {
-  marca: 'Fiat',
-  ano: 2018,
-  portas: 4,
+// Extraia o backgroundColor, color e margin do btn
+const btn = document.querySelector('button');
+const btnStyles = getComputedStyle(btn);
+const { backgroundColor, color, margin } = btnStyles;
+console.log(backgroundColor);
+console.log(color);
+console.log(margin);
+
+// Troque os valores das variáveis abaixo
+let cursoAtivo = 'JavaScript';
+let cursoInativo = 'HTML';
+[cursoAtivo, cursoInativo ] = [cursoInativo, cursoAtivo];
+
+console.log(cursoAtivo);
+console.log(cursoInativo);
+
+// Corrija o erro abaixo
+const cachorro = {
+  nome: 'Bob',
+  raca: 'Labrador',
+  cor: 'Amarelo'
 }
 
-const {marca, ano} = carro;
-
-console.log(marca); // Fiat
-console.log(ano); // 2018
-
-// const cliente = {
-//   nome: 'Andre',
-//   compras: {
-//     digitais: {
-//       livros: ['Livro 1', 'Livro 2'],
-//       videos: ['Video JS', 'Video HTML']
-//     },
-//     fisicas: {
-//       cadernos: ['Caderno 1']
-//     }
-//   }
-// }
-
-// console.log(cliente.compras.digitais.livros);
-// console.log(cliente.compras.digitais.videos);
-
-// const {livros, videos} = cliente.compras.digitais;
-
-// console.log(livros);
-// console.log(videos);
-
-const cliente = {
-  nome: 'Andre',
-  compras: {
-    digitais: {
-      livros: ['Livro 1', 'Livro 2'],
-      videos: ['Video JS', 'Video HTML']
-    },
-    fisicas: {
-      cadernos: ['Caderno 1']
-    }
-  }
-}
-
-const {fisicas, digitais, digitais: {livros, videos}} = cliente.compras;
-
-console.log(fisicas);
-console.log(livros);
-console.log(videos);
-console.log(digitais);
-
-const {nome, compras} = cliente;
-// ou
-const {nome: nomeCliente, compras: comprasCliente} = cliente;
-const { email = 'email@gmail.com', cpf} = cliente;
-
-
-const frutas = ['Banana', 'Uva', 'Morango'];
-
-const primeiraFruta = frutas[0];
-const segundaFruta = frutas[1];
-const terceiraFruta = frutas[2];
-
-// Com destructuring
-const [primeira, segunda, terceira] = frutas;
-
-
-function handleKeyboard({key}) {
-  console.log(key);
-}
-
-document.addEventListener('keyup', handleKeyboard);
+const {cor: bobCor} = cachorro;
+console.log(bobCor);
